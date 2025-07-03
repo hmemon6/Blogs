36820faeea2ec24dad9cd2fc321234750c54d68f
haseeb <haseeb.ashfaque97@hotmail.com>
2025-07-02 23:21:12 -0600

    modified vercel.json

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 pip install -r requirements.txt
-
-
-
 python manage.py collectstatic --no-input --clear
 python manage.py makemigrations
 python manage.py migrate
-
-
 python -m uvicorn blog_project.asgi:application
