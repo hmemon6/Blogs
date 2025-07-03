774078f8574eda4a01b65e79b3788e0b1e3c059c
haseeb <haseeb.ashfaque97@hotmail.com>
2025-07-02 23:52:50 -0600

    a

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 python manage.py makemigrations
 python manage.py migrate
-python -m uvicorn blog_project.asgi:application
+python -m gunicorn blog_project.wsgi:application
