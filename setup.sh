7600c86b0784b0a05f1bbb879fb3baa0065d4567
haseeb <haseeb.ashfaque97@hotmail.com>
2025-07-04 11:27:53 -0600

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ python manage.py collectstatic --no-input --clear
 python manage.py makemigrations
 python manage.py migrate
 python -m gunicorn blog_project.wsgi:application
+
+# copies stuff froms taticfiles generated during collecstatic command to static. If you dont have static folder
+# use this command first mkdir -p static
+cp -r staticfiles/* static/
