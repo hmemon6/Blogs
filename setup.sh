71e3c21a9f680a546a021941c83414b39a609229
haseeb <haseeb.ashfaque97@hotmail.com>
2025-07-04 11:42:26 -0600

    changed order of commands in .sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
+# copies stuff froms taticfiles generated during collecstatic command to static. If you dont have static folder
+# use this command first mkdir -p static
+cp -r staticfiles/* static/
 python manage.py makemigrations
 python manage.py migrate
 python -m gunicorn blog_project.wsgi:application
 
-# copies stuff froms taticfiles generated during collecstatic command to static. If you dont have static folder
-# use this command first mkdir -p static
-cp -r staticfiles/* static/
+
