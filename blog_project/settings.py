080c54d6f73d8be20c45cabff00b34902291e962
haseeb <haseeb.ashfaque97@hotmail.com>
2025-07-04 11:36:08 -0600

    modifed settings.py for static_root and url

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_FILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATIC_FILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # STORAGES = {
 #     # ...
